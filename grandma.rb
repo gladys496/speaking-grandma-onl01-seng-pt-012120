42796de8d0f6efd9ea20ba1563adb3c860f15936
Gladys Gutierrez <gutierrezgladys721@gmail.com>
2020-02-22 03:54:44 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -15,15 +15,15 @@
 speak_to_grandma = "I love you grandma!".upcase
 
 if speak_to_grandma == "I love you grandma!".upcase
-  puts "I love you too pumpkin!".upcase
+  return "I love you too pumpkin!".upcase
  elsif speak_to_grandma == "Hi Nana, how are you?"
- puts "Huh?! Speak up, Sonny!".upcase
+ return  "Huh?! Speak up, Sonny!".upcase
  elsif speak_to_grandma == "Hi!"
- puts "Huh?! Speak up, Sonny!".upcase
+ return"Huh?! Speak up, Sonny!".upcase
  elsif speak_to_grandma == "What did you eat today?".upcase
-   puts "No, not since 1938!".upcase
+   return "No, not since 1938!".upcase
   else speak_to_grandma == "WHAT?" 
-    puts "No, not since 1938!".upcase
+    return "No, not since 1938!".upcase
   end 
   
   
